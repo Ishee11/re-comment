c12f956d14619870cc59199438d683d3bd59be46
Stepan Semenov <ishee11@gmail.com>
2025-11-17 21:51:04 +1000

    комментарии для entity

--- internal/entity/comment.go
+++ internal/entity/comment.go
@@ -6,9 +6,12 @@ import (
 	"time"
 )
 
+// ErrEmptyCommentText и ErrNotAuthor ошибки пустого комментария и неверного автора
 var ErrEmptyCommentText = errors.New("text is empty")
 var ErrNotAuthor = errors.New("not author")
 
+// Comment представляет комментарий пользователя к какой-либо сущности.
+// Внутри хранит ID комментария, ID пользователя, текст, дату создания и изменения.
 type Comment struct {
 	ID        int64
 	EntityID  int64
@@ -18,21 +21,7 @@ type Comment struct {
 	UpdatedAt time.Time
 }
 
-// update comment
-func (c *Comment) UpdateCommentText(userID int64, text string) error {
-	if userID != c.UserID {
-		return ErrNotAuthor
-	}
-	text = strings.TrimSpace(text)
-	if text == "" {
-		return ErrEmptyCommentText
-	}
-	c.Text = text
-	c.UpdatedAt = time.Now().UTC()
-	return nil
-}
-
-// create comment
+// NewComment валидация и создание комментария
 func NewComment(entityID, userID int64, text string) (*Comment, error) {
 	text = strings.TrimSpace(text)
 	if text == "" {
@@ -48,4 +37,18 @@ func NewComment(entityID, userID int64, text string) (*Comment, error) {
 		CreatedAt: now,
 		UpdatedAt: now,
 	}, nil
+}
+
+// UpdateCommentText валидация, изменения текста и даты обновления комментария
+func (c *Comment) UpdateCommentText(userID int64, text string) error {
+	if userID != c.UserID {
+		return ErrNotAuthor
+	}
+	text = strings.TrimSpace(text)
+	if text == "" {
+		return ErrEmptyCommentText
+	}
+	c.Text = text
+	c.UpdatedAt = time.Now().UTC()
+	return nil
 }
