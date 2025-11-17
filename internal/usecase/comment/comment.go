6b61779b314f4a432c66f4387d33446458fb5c5c
Stepan Semenov <ishee11@gmail.com>
2025-11-17 21:29:22 +1000

    add comment use-case

--- internal/usecase/comment/comment.go
+++ internal/usecase/comment/comment.go
@@ -1,1 +1,36 @@
 package comment
+
+import (
+	"github.com/evrone/go-clean-template/internal/entity"
+	"github.com/evrone/go-clean-template/internal/repo"
+)
+
+type UseCase struct {
+	repo repo.CommentRepository
+}
+
+func (uc *UseCase) CreateComment(userID int64, entityID int64, text string) (*entity.Comment, error) {
+	c, err := entity.NewComment(entityID, userID, text)
+	if err != nil {
+		return nil, err
+	}
+	if err = uc.repo.CreateComment(c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func (uc *UseCase) UpdateComment(id int64, userID int64, text string) error {
+	c, err := uc.repo.GetCommentByID(id)
+	if err != nil {
+		return err
+	}
+	if err = c.UpdateCommentText(userID, text); err != nil {
+		return err
+	}
+	return uc.repo.UpdateComment(c)
+}
+
+func (uc *UseCase) ListComments(entityID int64, sortAsc bool) ([]*entity.Comment, error) {
+	return uc.repo.ListCommentByEntity(entityID, sortAsc)
+}
